f9fe8bb78ede54d01e329b67db14172e4078e0e0
Acespace <alston.clark17@gmail.com>
2013-12-07 22:31:27 -0500

    Updating starter code
    This is messy. But for time sake, lets leave it.
    Regularly, I would have separate files for these different tasks so main would have like 5 lines of code.

--- myVision.py
+++ myVision.py
@@ -21,6 +21,27 @@ def swell(button, base):
 
 
     return button,
+
+
+
+def swell(bl, pos):
+
+    x,y = pos
+
+    for b in bl:
+        if b.collidepoint(x,y):
+            b.inflate(30,30)
+
+            
+
+
+    
+
+
+
+
+    
+
     
 def main():
 
@@ -53,34 +74,68 @@ def main():
 
 
 
-    #makes Green button
+    #red button
+    ButtonList=[]
     redButton = pygame.Rect(100, 500, 90, 65 )
+    ButtonList.append(redButton)
     pygame.draw.rect(screen, (100,0,0), redButton)
 
-    #makes
+    #green button
     greenButton = pygame.Rect(250, 500, 90, 65 )
+    ButtonList.append(greenButton)
     pygame.draw.rect(screen, (0,100,0), greenButton)
 
-
+    #blue button 
     blueButton = pygame.Rect(400, 500, 90, 65 )
+    ButtonList.append(blueButton)
     pygame.draw.rect(screen, (0,0,100), blueButton)
 
 
 
-   
-
     
-    
-
+    mousePos = pygame.mouse.get_pos()
     
 
 
+            
+            
+
     
+    #xBound = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90)
+    #yBound=()
+    
+        
     while True :
         pygame.display.update()
+        swell(ButtonList,mousePos)
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+
+            
+            
+
+
+            
+
+                print "hello"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             
 if __name__ == '__main__': main()
